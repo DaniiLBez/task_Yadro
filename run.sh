5c4a99aa5602b810fc27d5df55cd624bf542f720
DaniiLBez <dvbezzubov@stud.etu.ru>
2023-06-30 00:14:35 +0300

    [Improvements] corrected run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 docker run --rm \
     -u 1000:1000 \
     -v "$(pwd):/tmp/task_Yadro" \
-    dev:task_Yadro &&\
+    dev:task_Yadro \
     bash -c \
-    "$(pwd)/cmake_build/src/shr_mem_writer/shr_mem_writer | \
-    $(pwd)/cmake_build/src/shr_mem_reader/shr_mem_reader"
+    "/tmp/task_Yadro/cmake_build/src/shr_mem_writer/shr_mem_writer | \
+    /tmp/task_Yadro/cmake_build/src/shr_mem_reader/shr_mem_reader"
